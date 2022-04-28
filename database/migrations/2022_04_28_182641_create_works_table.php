70c61851c3383dba8f250d116fa6784e204b81a9
muratenes <m9urat@gmail.com>
2022-04-28 23:09:45 +0300

    created work model and migration

---
+++ database/migrations/2022_04_28_182641_create_works_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('task_id');
+            $table->unsignedTinyInteger('week_number');
+            // TODO : SİL
+            $table->unsignedTinyInteger('hour');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
